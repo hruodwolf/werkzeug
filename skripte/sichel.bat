c344b5c61ab4e350ac606705c74b083f6104bbba
Rolloff, Artur <Artur.Rolloff@opitz-consulting.com>
2018-01-20 19:46:35 +0100

    sicherung a und b sind fertig

--- skripte/sichel.bat
+++ skripte/sichel.bat
@@ -1,11 +1,15 @@
 @echo off
-echo starte die sicherung..
+echo ===============================================================
+echo starte sicherung A
 
 set quelle=g:\
 set ziel=e:\
 
 set quelleExistiert=false
 set zielExistiert=false
+
+echo quelle - %quelle% %quelleExistiert%
+echo ziel - %ziel% %zielExistiert%
 
 if exist %quelle% (
   echo laufwerk %quelle% existiert
@@ -18,14 +22,47 @@ if exist %ziel% (
   echo laufwerk %ziel% existiert
   set zielExistiert=true
 ) else (
-  echo laufwerk %ziel% existiert nicht
+  echo laufwerk %ziel% existiert NICHT
 )
 
 if "%quelleExistiert%;%zielExistiert%"=="true;true" ( 
     echo Beide laufwerke existieren, beginne mit der sicherung
     robocopy %quelle%daten %ziel%sicherung /MIR /LOG+:"%quelle%werkzeugkasten\robocopy\logs\%date%_sicherung.log" /TEE
 ) else ( 
-    echo mindestens eines von beiden - %quelle% %quelleExistiert% oder %ziel% %zielExistiert% - existiert nicht, sicherung wird abgebrochen
+    echo mindestens eines von beiden - %quelle% %quelleExistiert% oder %ziel% %zielExistiert% - existiert NICHT, sicherung wird abgebrochen
+)
+
+echo ===============================================================
+echo starte sicherung B
+
+set quelle=a:\
+set ziel=b:\
+
+set quelleExistiert=false
+set zielExistiert=false
+
+echo quelle - %quelle% %quelleExistiert%
+echo ziel - %ziel% %zielExistiert%
+
+if exist %quelle% (
+  echo laufwerk %quelle% existiert
+  set quelleExistiert=true
+) else (
+  echo laufwerk %quelle% existiert NICHT
+)
+
+if exist %ziel% (
+  echo laufwerk %ziel% existiert
+  set zielExistiert=true
+) else (
+  echo laufwerk %ziel% existiert NICHT
+)
+
+if "%quelleExistiert%;%zielExistiert%"=="true;true" ( 
+    echo Beide laufwerke existieren, beginne mit der sicherung
+    robocopy %quelle%daten %ziel%sicherung /MIR /LOG+:"%quelle%logs\%date%_sicherung.log" /TEE
+) else ( 
+    echo mindestens eines von beiden - %quelle% %quelleExistiert% oder %ziel% %zielExistiert% - existiert NICHT, sicherung wird abgebrochen
 )
 
 echo ende..
