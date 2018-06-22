de3720f0c06da418816bcb8a53f001ba32b82c3a
Rolloff <Artur.Rolloff@opitz-consulting.com>
2018-06-22 14:13:02 +0200

    zwischenstand

--- skripte/erstelle_trigger.sql
+++ skripte/erstelle_trigger.sql
@@ -4,14 +4,15 @@ set linesize 200
 
 spool C:\tmp\erstelle_trigger_skripte.sql
 
+
 SELECT
       'set feedback off' || chr(10)
   ||  'set head off' || chr(10)
-  ||  'set linesize 120' || chr(10)
+  ||  'set linesize 200' || chr(10)
   FROM DUAL;
 
 SELECT 
-      'spool C:\tmp\TRG_' || Synonym_Name || '.sql' || chr(10)
+      'spool C:\tmp\trg_' || Synonym_Name || '_bri.sql' || chr(10)
   ||  'SELECT ' || chr(10)
   ||  '   rpad(chr(32),0) ||  ''CREATE OR REPLACE TRIGGER TRG_' || Synonym_Name || '_BRI''' || '|| chr(10)' || chr(10)
   ||  '|| rpad(chr(32),2) ||  ''BEFORE INSERT'' || chr(10)' || chr(10)
@@ -20,7 +21,7 @@ SELECT
   ||  '|| rpad(chr(32),0) ||  ''BEGIN'' || chr(10)' || chr(10)
   ||  '|| rpad(chr(32),2) ||  ''IF :NEW.' || Column_Name || ' IS NULL'' || chr(10)' || chr(10)
   ||  '|| rpad(chr(32),2) ||  ''THEN'' || chr(10)' || chr(10)
-  ||  '|| rpad(chr(32),4) ||  '':NEW.' || Column_Name || ' := SEQ_GLOBAL_ID.NEXTVAL;'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),4) ||  '':NEW.' || Column_Name || ' := ' || Synonym_Name || '_SEQ.NEXTVAL;'' || chr(10)' || chr(10)
   ||  '|| rpad(chr(32),2) ||  ''END IF;'' || chr(10)' || chr(10)
   ||  '|| rpad(chr(32),0) ||    chr(10)' || chr(10)
   ||  '|| rpad(chr(32),2) ||  '':NEW.INS_USER := NVL (:NEW.INS_USER, BENUTZERVERWALTUNG.HOLE_USERNAMEN);'' || chr(10)' || chr(10) 
@@ -28,6 +29,32 @@ SELECT
   ||  '|| rpad(chr(32),2) ||  '':NEW.UPD_USER := NVL (:NEW.UPD_USER, BENUTZERVERWALTUNG.HOLE_USERNAMEN);'' || chr(10)' || chr(10)
   ||  '|| rpad(chr(32),2) ||  '':NEW.UPD_DATE := SYSDATE;'' || chr(10)' || chr(10)
   ||  '|| rpad(chr(32),2) ||  '':NEW.VERSION  := NVL (:NEW.VERSION, 0);'' || chr(10)' || chr(10)  
+  ||  '|| rpad(chr(32),0) ||  ''END;'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),0) ||  ''/''' || chr(10)
+  ||  'FROM DUAL;' || chr(10)
+  ||  'spool off;' || chr(10)
+  ||    'spool C:\tmp\trg_' || Synonym_Name || '_bru.sql' || chr(10)
+  ||  'SELECT ' || chr(10)
+  ||  '   rpad(chr(32),0) ||  ''CREATE OR REPLACE TRIGGER TRG_' || Synonym_Name || '_BRU''' || '|| chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''BEFORE UPDATE'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''ON ' || Table_Name || ''' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''FOR EACH ROW'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),0) ||  ''BEGIN'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''IF NOT UPDATING (''''UPD_USER'''') OR :NEW.UPD_USER IS NULL'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''THEN'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  '':NEW.UPD_USER := BENUTZERVERWALTUNG.HOLE_USERNAMEN;'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''END IF;'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),0) ||    chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''IF NOT UPDATING (''''UPD_DATE'''') OR :NEW.UPD_DATE IS NULL'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''THEN'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  '':NEW.UPD_DATE := SYSDATE;'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''END IF;'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),0) ||    chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''IF NOT UPDATING (''''VERSION'''') OR :NEW.VERSION IS NULL'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''THEN'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  '':NEW.VERSION := :OLD.VERSION + 1;'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),2) ||  ''END IF;'' || chr(10)' || chr(10)
+  ||  '|| rpad(chr(32),0) ||    chr(10)' || chr(10)
   ||  '|| rpad(chr(32),0) ||  ''END;'' || chr(10)' || chr(10)
   ||  '|| rpad(chr(32),0) ||  ''/''' || chr(10)
   ||  'FROM DUAL;' || chr(10)
@@ -46,10 +73,10 @@ SELECT
           FROM User_Tables Tabs
                JOIN User_Constraints Cons USING (Table_Name)
                JOIN User_Cons_Columns Cols USING (Constraint_Name, Table_Name)
-         WHERE Cons.Constraint_Type = 'P' and table_name in ('EMPFAENGER', 'ERGEBNIS_P010'));
+         WHERE Cons.Constraint_Type = 'P' and table_name in ('VERURSACHER_FEHLERART'));
          
 spool off;
 
 @C:\tmp\erstelle_trigger_skripte.sql
-/         
+/
 
